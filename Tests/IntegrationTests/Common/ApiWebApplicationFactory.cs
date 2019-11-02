eefb7a1597611e949d3ac14a8d4e86ec60a75bad
rileykacmarynski <riley.kacmarynski@gmail.com>
2019-11-02 18:50:37 -0500

    get application factory set up for integration tests.

---
+++ Tests/IntegrationTests/Common/ApiWebApplicationFactory.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Linq;
+using System.Net.Http;
+using Infrastructure.Database;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace IntegrationTests.Common
+{
+    public class ApiWebApplicationFactory<TStartup> : WebApplicationFactory<TStartup> where TStartup : class
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services => {
+                // remove already registered dbcontext and replace it with in-memory
+                var descriptor = services.SingleOrDefault(d => d.ServiceType == typeof(DbContextOptions<ThingShopDbContext>));
+                if(descriptor != null) services.Remove(descriptor);
+
+                services.AddDbContext<ThingShopDbContext>((options, context) => {
+                    context.UseInMemoryDatabase("IntegrationTestDb");
+                });
+
+                #region If we want to seed a test database we can do that here
+                    // var sp = services.BuildServiceProvider();
+                    // using(var scope = sp.CreateScope())
+                    // {
+                    //     var scopedServices = scope.ServiceProvider;
+                    //     var db = scopedServices.GetRequiredService<ThingShopDbContext>();
+
+                    //     db.Database.Migrate();
+
+                    //     try 
+                    //     {
+                    //         // run code to seed database here
+                    //     }
+                    //     catch(Exception ex)
+                    //     {
+                    //         // handle exception
+                    //     }
+                    // }
+                #endregion
+
+            });
+
+        }
+
+        protected override void ConfigureClient(HttpClient client)
+        {
+            // configure client here
+        }
+    }
+}
---
+++ Tests/IntegrationTests/Common/BaseApiTest.cs
@@ -0,0 +1,41 @@
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Newtonsoft.Json;
+using NUnit.Framework;
+using Web;
+
+namespace IntegrationTests.Common
+{
+    [TestFixture]
+    public class BaseApiTest
+    {
+        private HttpClient _client;
+        private WebApplicationFactory<Startup> _factory;
+
+        [OneTimeSetUp]
+        public void Init()
+        {
+            _factory = new ApiWebApplicationFactory<Startup>();
+            _client = _factory.CreateClient();
+        }
+
+        [OneTimeTearDown]
+        public void TearDown()
+        {
+            _client.Dispose();
+            _factory.Dispose();
+        }
+
+        protected async Task<HttpResponseMessage> PostAsync(string url, object values)
+        {
+            var json = JsonConvert.SerializeObject(values);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            return await _client.PostAsync(url, content).ConfigureAwait(false);
+        }
+
+        protected async Task<HttpResponseMessage> GetAsync(string url) =>
+            await _client.GetAsync(url).ConfigureAwait(false);
+    }
+}
